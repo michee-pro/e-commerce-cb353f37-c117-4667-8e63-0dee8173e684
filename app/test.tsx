e92ddd3a1761aa40ca7cb9063702bd27f9930d93
michee-pro <voteprojet@gmail.com>
2024-08-05 13:28:47 +0000

    Initial commit

---
+++ app/test.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+
+function TestProject() {
+  return (
+    <div>
+      <h1>Le travail libere l'homme de la pauvrete</h1>
+    </div>
+  )
+}
+
+export default TestProject
